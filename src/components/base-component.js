9276a85405b259c51e99dcd28a4382ba0818d924
Maria Betman <maria.betman@valtech.com>
2026-08-03 14:54:59 -0300

    refactor(components): adota reset e tipografia globais no shadow dom de todo componente

--- src/components/base-component.js
+++ src/components/base-component.js
@@ -1,7 +1,18 @@
+import reset from '../styles/reset.css?inline';
+import typograph from '../styles/typograph.css?inline';
+
+const resetStylesheet = new CSSStyleSheet();
+resetStylesheet.replaceSync(reset);
+
+const typographStylesheet = new CSSStyleSheet();
+typographStylesheet.replaceSync(typograph);
+
 export class BaseComponent extends HTMLElement {
   constructor({ template = '', styles = '' } = {}) {
     super();
     this.attachShadow({ mode: 'open' });
+
+    this.shadowRoot.adoptedStyleSheets = [resetStylesheet, typographStylesheet];
 
     if (styles) {
       this.#adoptStylesheet(styles);
@@ -15,7 +26,10 @@ export class BaseComponent extends HTMLElement {
   #adoptStylesheet(cssText) {
     const stylesheet = new CSSStyleSheet();
     stylesheet.replaceSync(cssText);
-    this.shadowRoot.adoptedStyleSheets = [stylesheet];
+    this.shadowRoot.adoptedStyleSheets = [
+      ...this.shadowRoot.adoptedStyleSheets,
+      stylesheet,
+    ];
   }
 
   $(selector) {
