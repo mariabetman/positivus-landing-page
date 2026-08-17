a4c8de5bdbe404a0104588fae51eb1daf0d31c37
Maria Betman <maria.betman@valtech.com>
2026-08-17 14:47:37 -0300

    feat: adiciona import de composição em positivus-card-list

--- src/components/organisms/positivus-card-list/positivus-card-list.js
+++ src/components/organisms/positivus-card-list/positivus-card-list.js
@@ -1,6 +1,7 @@
 import { BaseComponent } from '../../base-component.js';
 import template from './positivus-card-list.html?raw';
 import styles from './positivus-card-list.css?inline';
+import '../../molecules/positivus-example-card/positivus-example-card.js';
 
 export class PositivusCardList extends BaseComponent {
   static observedAttributes = BaseComponent.extractPropNames(template);
