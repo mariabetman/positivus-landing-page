ff9838e35cd50a634833edea03c71d484e60d8e4
Maria Betman <maria.betman@valtech.com>
2026-08-17 14:46:45 -0300

    feat: gera js, storybook e teste de positivus-title-and-description

---
+++ src/components/molecules/positivus-title-and-description/positivus-title-and-description.stories.js
@@ -0,0 +1,10 @@
+import './positivus-title-and-description.js';
+
+export default {
+  title: 'Molecules/PositivusTitleAndDescription',
+  tags: ['autodocs'],
+};
+
+export const Default = {
+  render: () => document.createElement('positivus-title-and-description'),
+};
