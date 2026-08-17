3e50df4ad172cf5555856c8c9e5f4cd30c1898bf
Maria Betman <maria.betman@valtech.com>
2026-08-17 13:38:00 -0300

    feat: adiciona import de imagens em positivus-example-card

--- src/components/molecules/positivus-example-card/positivus-example-card.js
+++ src/components/molecules/positivus-example-card/positivus-example-card.js
@@ -1,14 +1,16 @@
 import { BaseComponent } from '../../base-component.js';
-import template from './positivus-example-card.html?raw';
+import rawTemplate from './positivus-example-card.html?raw';
 import styles from './positivus-example-card.css?inline';
 import imgExample from './images/example.svg';
 
+const template = rawTemplate
+  .replaceAll('./images/example.svg', imgExample);
+
 export class PositivusExampleCard extends BaseComponent {
+  static observedAttributes = BaseComponent.extractPropNames(template);
+
   constructor() {
     super({ template, styles });
-    this.$$('img[src="./images/example.svg"]').forEach((img) => {
-      img.src = imgExample;
-    });
   }
 }
 
